72cd13905b6426340969ce936e869626a92f4192
jobarne <johan.barne@gmail.com>
2019-01-02 14:19:52 +0100

    Add files via upload

---
+++ bin/db.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect('localhost','ssf_wxX','ssf_yyyy','ssf_wxX')
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
